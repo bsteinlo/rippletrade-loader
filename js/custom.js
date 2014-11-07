3b67813ab8bc5e35e317080f621c763d1f6393b7
Ben Stein-Lobovits <ben@ripple.com>
2014-11-07 11:31:41 -0800

    add login

--- js/custom.js
+++ js/custom.js
@@ -6,4 +6,8 @@ $(".initial").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd
 setTimeout(function() {
      $('.loading').hide();
      $('.loaded').show();
+     $('.login_top').fadeIn(1000);  
+	 $('.login_placeholder').fadeIn(1500);   
+	 $('.footer_placeholder').fadeIn(2000);  
+	 $('.site-wrapper').css("background", "#fff");  
 }, 5000);
