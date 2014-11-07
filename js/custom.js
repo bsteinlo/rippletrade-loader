679239be8920e74125e413212758e8ee35109d92
Ben Stein-Lobovits <ben@ripple.com>
2014-11-07 12:39:53 -0800

    add comments

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,16 @@
+// initial animation ends, swap out for spinning logo
 $(".initial").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
 	$(this).hide();	
 	$('.loading').show();
 });
 
+//this is arbitrary, im assuming it will be passed as a callback when client loads
 setTimeout(function() {
     $('.loading').hide();
     $('.loaded').show();     
 }, 5000);
 
-
+//final animation ends, fade in placeholders
 $(".loaded").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
 	$('.login_top').fadeIn(500);  
 	$('.login_placeholder').fadeIn(1000);   
