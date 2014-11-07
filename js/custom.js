2e6c7b94727fe551e43d1c82df22d43551e45a7a
Ben Stein-Lobovits <ben@ripple.com>
2014-11-07 14:03:55 -0800

    [FIX] cross browser

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,22 @@
 // initial animation ends, swap out for spinning logo
-$(".initial").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+$(".initial").bind("animationend webkitAnimationEnd mozAnimationEnd oAnimationEnd MSAnimationEnd", function() {
 	$(this).hide();	
 	$('.loading').show();
 });
 
 //this is arbitrary, im assuming it will be passed as a callback when client loads
 setTimeout(function() {
+   finalLoad();
     $('.loading').hide();
-    $('.loaded').show();     
+    $('.loaded').toggle();     
+    
 }, 5000);
 
 //final animation ends, fade in placeholders
-$(".loaded").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
-	$('.login_top').fadeIn(500);  
-	$('.login_placeholder').fadeIn(1000);   
-	$('.footer_placeholder').fadeIn(1500);  
-});
+function finalLoad() {
+	$(".loaded").bind("animationend webkitAnimationEnd mozAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+		$('.login_top').fadeIn(500);  
+		$('.login_placeholder').fadeIn(1000);   
+		$('.footer_placeholder').fadeIn(1500);  
+	});
+}
