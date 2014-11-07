93602ffbd244104aba0869c541346c18359fc6e3
Ben Stein-Lobovits <ben@ripple.com>
2014-11-07 11:46:29 -0800

    update animation

--- js/custom.js
+++ js/custom.js
@@ -4,10 +4,15 @@ $(".initial").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd
 });
 
 setTimeout(function() {
-     $('.loading').hide();
-     $('.loaded').show();
-     $('.login_top').fadeIn(1000);  
-	 $('.login_placeholder').fadeIn(1500);   
-	 $('.footer_placeholder').fadeIn(2000);  
-	 $('.site-wrapper').css("background", "#fff");  
-}, 5000);
+    $('.loading').hide();
+    $('.loaded').show();     
+}, 3000);
+
+
+$(".loaded").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function() {
+	$('.bg').removeClass('blue'); 
+	$('.login_top').fadeIn(1000);  
+	$('.login_placeholder').fadeIn(1500);   
+	$('.footer_placeholder').fadeIn(2000);  
+	$('.site-wrapper').css("background", "#fff");  
+});
